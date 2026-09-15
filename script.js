13e71d38060c67b82742c9ab22fa93a3dd2723f9
dindaww <madedinda251@gmail.com>
2026-09-15 20:20:00 +0800

    Update portofolio

--- script.js
+++ script.js
@@ -5,42 +5,45 @@ const texts = [
     "I love Ice Cream.",
 ];
 
-let textIndex = 0;
-let charIndex = 0;
-let deleting = false;
+ let textIndex = 0;
+    let charIndex = 0;
+    let deleting = false;
 
-function typeEffect() {
-    const currentText = texts[textIndex];
+    function typeEffect() {
+        const currentText = texts[textIndex];
 
-    if (!deleting) {
-        typingText.textContent = currentText.slice(0, charIndex + 1);
-        charIndex++;
+        if (!deleting) {
+            typingText.textContent = currentText.slice(0, charIndex + 1);
+            charIndex++;
 
-        if (charIndex === currentText.length) {
-            deleting = true;
-            setTimeout(typeEffect, 1500);
-            return;
+            if (charIndex === currentText.length) {
+                deleting = true;
+                setTimeout(typeEffect, 1500);
+                return;
+            }
+        } else {
+              typingText.textContent = currentText.slice(0, charIndex - 1);
+            charIndex--;
+
+            if (charIndex === 0) {
+                deleting = false;
+                textIndex = (textIndex + 1) % texts.length;
+            }
         }
-    } else {
-        typingText.textContent = currentText.slice(0, charIndex - 1);
-        charIndex--;
 
-        if (charIndex === 0) {
-            deleting = false;
-            textIndex = (textIndex + 1) % texts.length;
-        }
+        setTimeout(typeEffect, deleting ? 50 : 90);
     }
 
-    setTimeout(typeEffect, deleting ? 50 : 90);
-}
+    typeEffect();
 
-typeEffect();
-
-
-/* Mobile Navbar */
+// Mobile Navbar
 const menuButton = document.getElementById("menu-button");
 const mobileMenu = document.getElementById("mobile-menu");
 
-menuButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-});
+if (menuButton && mobileMenu) {
+
+    menuButton.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
+
+}
